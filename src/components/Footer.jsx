a0170d61440e035aedc01960902a14287f2225e9
Md Ashraful Islam <md_ashraful@icloud.com>
2023-05-10 11:20:16 +1000

    changes to icons render methods

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
-import React, { useRef, createRef } from 'react'
+import React from 'react'
 import { FaBehance, FaDribbble, FaInstagram } from "react-icons/fa";
 import { GrLinkedinOption } from "react-icons/gr";
-import Hoverbutton from './Hoverbutton';
+
 
 
 const social = [
@@ -24,13 +24,13 @@ const social = [
 ]
 
 const Footer = () => {
-  // const references = useRef(social.map(item => createRef(item)));
+
   return (
     <div className="footer_container">
       <div className="email_me">
-        {/* <Hoverbutton name="Let's Talk" /> */}
+
         <a>Let's Talk</a>
-        {/* <img className="send_svg" src="./assets/images/send.svg" alt="send icon" /> */}
+
         <svg width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M17.083 18.4133H2.91634" stroke="#292D32" strokeWidth="1.8" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round" />
           <path d="M15.833 2.99683L4.16634 14.6635" stroke="#292D32" strokeWidth="1.8" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round" />
@@ -38,22 +38,39 @@ const Footer = () => {
         </svg>
       </div>
       <div className="social_media">
-        {
+        <div
+          id="GrLinkedinOption"
+          alt="GrLinkedinOption"
+          className='icons'
+          onClick={() => window.open("https://www.linkedin.com/in/mdashiqdesigner/", '_blank')}
+        >
+          <GrLinkedinOption />
+        </div>
+        <div
+          id="FaBehance"
+          alt="FaBehance"
+          className='icons'
+          onClick={() => window.open("https://www.behance.net/mdashiq_designer", '_blank')}
+        >
+          <FaBehance />
+        </div>
+        <div
+          id="FaDribbble"
+          alt="FaDribbble"
+          className='icons'
+          onClick={() => window.open("https://dribbble.com/mdashiq_designer", '_blank')}
+        >
+          <FaDribbble />
+        </div>
+        <div
+          id="FaInstagram"
+          alt="FaInstagram"
+          className='icons'
+          onClick={() => window.open("https://www.behance.net/mdashiq_designer", '_blank')}
+        >
+          <FaInstagram />
+        </div>
 
-
-          social.map((item, index) => {
-            const IconComponent = eval(item.name);
-            return (<div
-              // ref={references.current[index]}
-              key={index}
-              id={item.name}
-              alt={item.name} className='icons'
-              onClick={() => window.open(item.url, '_blank')}
-            >
-              <IconComponent />
-            </div>)
-          })
-        }
       </div>
     </div>
   )
